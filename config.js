508c65a2e7a93e5fa0a27322ef4b9e7f7b4c5060
OcheniBliss <ocheniblessing16@gmail.com>
2025-04-04 16:35:41 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+ document.querySelector("#text-1").innerText="Congratulations, you have been selected as the winner of 100million🎉🎉!!!!"
+ document.getElementById("text-2").innerText=" Click the button to claim rewards  "
+ 
+ 
+ 
+ 
+ 
+ function display (){
+   document.querySelector("#msg").innerText="Go and learn a skill😏"
+ }
+ 
